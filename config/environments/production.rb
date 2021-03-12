4e0fa8d87ff19d7e7ff52f4265d35322d95293fc
jean-claude07 <jeclaude78@gmail.com>
2021-03-12 17:53:06 +0300

    env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  ruby ENV['CUSTOM_RUBY_VERSION'] || '3.0.0'
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
